513227c1f3a98492945035f9308403ad58e82bf2
Ali Almaamouri <143419457+alialm05@users.noreply.github.com>
2025-02-01 14:03:50 -0500

    main fixes complete

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const hamburgerMenu = document.querySelector(".hamburger-menu");
+    const hamburgerIcon = document.querySelector(".hamburger-icon");
+
+    hamburgerMenu.classList.toggle("show");
+    hamburgerIcon.classList.toggle("show");
+}
